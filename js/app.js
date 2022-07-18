cc2dfeed874e1413446df8062f43101c253814c4
daniel-dasdaf <danieldasdaf@gmail.com>
2022-07-19 00:21:43 +0100

    Finished

--- js/app.js
+++ js/app.js
@@ -1,13 +1,33 @@
 var allBtns = document.querySelectorAll('.rating__number');
 var sectionRating = document.querySelector('.rating');
-var sectionThanks = document.querySelector('.thanks')
+var sectionThanks = document.querySelector('.thanks');
+var ratingNumber = document.querySelector('.thanks__selected');
 
-function getBtn(data){
+function getBtn(data, number){
     console.log('clicked');
     [].forEach.call(allBtns, function(btn) {
         btn.classList.remove("selected");
     });
     data.classList.add("selected");
+    console.log(number)
+
+    switch(number){
+        case 1:
+            ratingNumber.innerHTML="You selected 1 out of 5"
+        break;
+        case 2:
+            ratingNumber.innerHTML="You selected 2 out of 5"
+        break;
+        case 3:
+            ratingNumber.innerHTML="You selected 3 out of 5"
+        break;
+        case 4:
+            ratingNumber.innerHTML="You selected 4 out of 5"
+        break;
+        case 5:
+            ratingNumber.innerHTML="You selected 5 out of 5"
+        break;
+    }
 }
 
 function submitRating(){
